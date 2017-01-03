784eab491e20fa80b9e4857a6167e1bf82f522bb
William Angélico <willangelico@gmail.com>
2017-01-03 13:07:19 -0200

    Javascript avançado - salvando em local storage

---
+++ js/config.js
@@ -0,0 +1,9 @@
+function setConfig(){
+	var texts = {
+		"title": "Shopping Control"
+	};
+	document.title = texts.title;
+	document.getElementById("navTitle").innerHtml = texts.title;
+}
+
+setConfig();
---
+++ js/main.js
@@ -0,0 +1,161 @@
+var list = [
+	{"desc":"rice","amount":"1","value":"5.40"},
+	{"desc":"beer","amount":"12","value":"1.99"},
+	{"desc":"meat","amount":"1","value":"15.00"}
+];
+
+
+function getTotal(list){
+	var total = 0;
+	for(var key in list){
+		total += list[key].value * list[key].amount;
+	}
+	document.getElementById("totalValue").innerHTML = formatValue(total);
+	return total;
+}
+
+function setList(list){
+	var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
+	for(var key in list){
+		table += '<tr><td>' + formatDesc(list[key].desc) + '</td><td>' + formatAmount(list[key].amount) + '</td><td>' + formatValue(list[key].value) + '</td><td><button onclick="setUpdate(' + key + ');" class="btn btn-default">Edit</button> <button onclick="deleteData(' + key + ');" class="btn btn-default">Delete</button></td></tr>';	
+	}
+	table += '</tbody>';
+	document.getElementById('listTable').innerHTML = table;
+	getTotal(list);
+	saveListStorage(list);
+}
+
+function formatDesc(desc){
+	var str = desc.toLowerCase();
+	str = str.charAt(0).toUpperCase() + str.slice(1);
+	return str;
+}
+
+function formatAmount(amount){
+	return parseInt(amount);
+}
+
+function formatValue(value){
+	var str = parseFloat(value).toFixed(2) + '';
+	str = str.replace('.',',');
+	str = '$ ' + str;
+	return str; 
+}
+
+function addData(){
+	if(!validation()){
+		return;
+	}
+	var desc = document.getElementById("desc").value;
+	var amount = document.getElementById("amount").value;
+	var value = document.getElementById("value").value;
+	list.unshift({ "desc" : desc ,"amount" : amount , "value" : value });
+	setList(list);
+}
+
+function setUpdate(id){
+	var obj = list[id];
+	document.getElementById("desc").value = obj.desc;
+	document.getElementById("amount").value = obj.amount;
+	document.getElementById("value").value = obj.value;
+	document.getElementById("btnUpdate").style.display = "inline-block";
+	document.getElementById("btnAdd").style.display = "none";
+
+	document.getElementById("inputIdUpdate").innerHTML = '<input id="idUpdate" type="hidden" value="' + id + '" />';
+}
+
+function resetForm(){
+	document.getElementById("desc").value = "";
+	document.getElementById("amount").value = "";
+	document.getElementById("value").value = "";
+	document.getElementById("btnUpdate").style.display = "none";
+	document.getElementById("btnAdd").style.display = "inline-block";
+
+	document.getElementById("inputIdUpdate").innerHTML = "";
+}
+
+function updatedata(){
+	if(!validation()){
+		return;
+	}
+	var id = document.getElementById("idUpdate").value;
+	var desc = document.getElementById("desc").value;
+	var amount = document.getElementById("amount").value;
+	var value = document.getElementById("value").value;
+
+	list[id] = { "desc" : desc ,"amount" : amount , "value" : value }
+
+	resetForm();
+
+	setList(list);
+}
+
+function deleteData(id){
+	if(confirm("Delete this item?")){
+		// if(id === list.length - 1){
+		// 	list.pop();
+		// }else if(id === 0){
+		// 	list.shift();
+		// }else{
+		// 	var arrAuxIni = list.slice(0,id);
+		// 	var arrAuxEnd = list.slice(id + 1);
+		// 	list = arrAuxIni.concat(arrAuxEnd)
+		// }
+		delete list[id];
+		setList(list);
+	}
+
+}
+
+function validation(){
+	var desc = document.getElementById("desc").value;
+	var amount = document.getElementById("amount").value;
+	var value = document.getElementById("value").value;
+	var errors = "";
+
+	if(desc === ""){
+		errors += "<p>Fill out Description</p>";
+	}
+	if(amount === ""){
+		errors += "<p>Fill out a quantity</p>";	
+	}else if(amount != parseInt(amount)){
+		errors += "<p>Fill out a valid amount</p>";
+	}
+	if(value === ""){
+		errors += "<p>Fill out value</p>";	
+	}else if(value != parseFloat(value)){
+		errors += "<p>Fill out a valid value</p>";
+	}
+
+	if(errors != ""){
+		document.getElementById("errors").style.padding = "10px 20px";		
+		document.getElementById("errors").innerHTML = "<h3>Error:</h3>" + errors;
+		return 0;
+	}else{
+		document.getElementById("errors").innerHTML = "";
+		return 1;
+	}
+}
+
+function deleteList(){
+	if(confirm("Delete this list?")){
+		list = [];
+	}
+	setList(list);
+}
+
+function saveListStorage(list){
+	var jsonStr = JSON.stringify(list);
+	localStorage.setItem("list",jsonStr);
+}
+
+function initListStorage(){
+	var testList = localStorage.getItem("list");
+	if(testList){
+		list = JSON.parse(testList);
+	}
+	setList(list);
+}
+
+
+initListStorage();
